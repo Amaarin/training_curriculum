7bb1d0653921e1ab03a0b0ffc7e07533245d807c
Amaarin <88422638+Amaarin@users.noreply.github.com>
2021-08-24 19:57:59 +0900

    Issue4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'calendars#index'
-  resources :calendars
+  root to: 'calendars#index'
+  resources :calendars do
+  end
 end
